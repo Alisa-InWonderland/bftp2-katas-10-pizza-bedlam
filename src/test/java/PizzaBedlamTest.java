0f0c6daa1d7dad4f1fcb3b5650680380bf8c171b
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-02-22 11:07:26 +0100

    clase pizza, 4 tests pasan

--- src/test/java/PizzaBedlamTest.java
+++ src/test/java/PizzaBedlamTest.java
@@ -1,3 +1,40 @@
+import product.Pizza;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PizzaBedlamTest {
 
+    @Test
+    void pizzaProscuttoEFunghi() {
+
+        assertThat(new Pizza("PROSCIUTTO E FUNGHI").getTicket(), equalTo("PROSCIUTTO E FUNGHI   12,50€\n" +
+                "   _____________________________\n" +
+                "   TOTAL                 12,50€"));
+    }
+
+    @Test
+    void pizzaMargherita() {
+
+        assertThat(new Pizza("MARGHERITA").getTicket(), equalTo("MARGHERITA   9,30€\n" +
+                "   _____________________________\n" +
+                "   TOTAL                 9,30€"));
+    }
+
+    @Test
+    void pizzaProscutto() {
+
+        assertThat(new Pizza("PROSCIUTTO").getTicket(), equalTo("PROSCIUTTO   12,00€\n" +
+                "   _____________________________\n" +
+                "   TOTAL                 12,00€"));
+    }
+
+    @Test
+    void pizza4STAGIONI() {
+
+        assertThat(new Pizza("4 STAGIONI").getTicket(), equalTo("4 STAGIONI   12,50€\n" +
+                "   _____________________________\n" +
+                "   TOTAL                 12,50€"));
+    }
 }
